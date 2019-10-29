d36b96cd1cbf15857f9feb9912ea219198eb6faa
nikolouzos <nickrnikolouzos@gmail.com>
2019-10-29 10:52:20 +0200

    Added loading the movies in the TopMoviesViewController - Customized the TopMoviesViewController - Minor layout fixes

--- Farmdrop Test/Controllers/TopMoviesViewController.swift
+++ Farmdrop Test/Controllers/TopMoviesViewController.swift
@@ -10,17 +10,62 @@ import UIKit
 
 class TopMoviesViewController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	var movies: Movies = []
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		// Get the top movies
+		// Change the title to large
+		navigationController?.navigationBar.prefersLargeTitles = true
+
+		// Remove the tableView footer
+		tableView.tableFooterView = UIView()
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		// Set the Networking delegate
+		Network.shared.delegate = self
+
+		// Get the top movies each time the VC comes into view
 		Network.shared.getTopMovies()
+	}
+}
+
+// MARK: - TableView Customization
+extension TopMoviesViewController: UITableViewDataSource, UITableViewDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return movies.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let cell = tableView.dequeueReusableCell(
+			withIdentifier: "movieOverviewCell", for: indexPath) as? MovieOverviewCell,
+			indexPath.row < movies.count {
+
+			let movie = movies[indexPath.row]
+
+			cell.setup(forMovie: movie)
+
+			return cell
+		}
+
+		return UITableViewCell()
 	}
 }
 
 // MARK: - Network Delegate
 extension TopMoviesViewController: NetworkDelegate {
 	func got(topMovies: TopMovies?, or networkError: NetworkError?) {
+		if let movies = topMovies?.movies {
 
+			// Set the movies
+			self.movies = movies
+
+			// Reload the tableView data
+			tableView.reloadData()
+		}
 	}
 }
